900b800f13765bc92172f84c1cc28a509d08a6a3
bassant-eng <66019119+bassant-eng@users.noreply.github.com>
2021-06-11 01:37:55 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,72 @@
+function validation(){
+    // store the value that user enter in variables
+    var user = document.getElementById('user').value;
+    var user2 =  document.getElementById('user2').value;
+    var pass = document.getElementById('pass').value;
+    var conpass = document.getElementById('conpass').value;
+    var email = document.getElementById('email').value;
+    var mobile = document.getElementById('mobile').value;
+    
+    
+    
+   
+   //  to accert a name of 3 letters at least
+    if(user.length<3){
+        alert("first Name must be more than 2 letters");
+        return false;
+    }
+    if(user2.length<3){
+      alert("last Name must be more than 2 letters");
+      return false;
+  }
+
+  var r=/01+[0-9]{9}/;
+  if(!(mobile.match(r))){
+      alert("enter valid mobile phone")
+  }
+    
+    if(pass.search(/[ ]/)!=-1){
+       alert("*must not include a space*");
+    return false;
+   }
+    // limit the lenght of the  pass
+    if(pass.length != 8 ){
+      alert("*please write a passward of 8 no long no less*");
+        return false;
+    }
+      // must enclud numbers
+   if(pass.search(/[0-9]/)==-1){
+        
+        alert("must incluce at least a number");
+        return false;
+    }
+    //not to accept without upper characters
+    var first= pass.substr(0,1);
+    if(!(first.match(/[A-Z]/))){
+        alert("first letter should be uppercase");
+        return false;
+    }
+   
+    // not to accept without lower characters
+   if(pass.search(/[a-z]/)==-1){
+        alert("*must include at least 1 lowwer character*");
+    return false;
+   }
+    // not accept without special characters
+    if(pass.search(/[!\@\#\$\%\^\&\*\(\)\_\-\=\+\<\>\,\?]/)==-1){
+       
+           alert("*must include 1 special character*");
+        return false;
+       }
+    
+    if(conpass != pass){
+        alert("**passward is not matches");
+        return false;
+    
+    }
+   
+      if(email.indexOf("@") == -1 || email.length < 6){
+      alert("*Please Enter valid Email*");
+       return false;
+  }
+}
